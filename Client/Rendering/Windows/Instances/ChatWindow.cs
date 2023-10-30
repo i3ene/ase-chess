007f0ac61de758766e9aecba6691c950a7ee1686
Benedikt Müll <benedikt.muell@zimmer-group.com>
2023-10-30 14:50:51 +0100

    implemented chat window

---
+++ Client/Rendering/Windows/Instances/ChatWindow.cs
@@ -0,0 +1,42 @@
+﻿using ase_chess.Client.Controls.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ase_chess.Client.Controls;
+
+namespace ase_chess.Client.Rendering.Windows.Instances
+{
+    public class ChatWindow : Window, ICommandable
+    {
+        public ChatWindow() : base(64, 8)
+        {
+
+        }
+
+        public override void addLine(string line)
+        {
+            int offset = border == BorderStyle.None ? 1 : 3;
+            if (lines.Count > height - offset)
+            {
+                lines.RemoveAt(1);
+            }
+            base.addLine(line);
+            NeedsUpdate();
+        }
+
+        public void OnCommand(Control sender, CommandArguments command)
+        {
+            if (command.arguments[0].ToLower() == "say")
+            {
+                string line = "";
+                for (int i = 1; i < command.arguments.Length; i++)
+                {
+                    line += command.arguments[i] + " ";
+                }
+                addLine(line.Trim());
+            }
+        }
+    }
+}
