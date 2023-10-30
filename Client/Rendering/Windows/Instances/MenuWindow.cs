0ad1f4f4eb25f034f9be8073e4d645054b6be569
Benedikt Müll <benedikt.muell@zimmer-group.com>
2023-10-30 14:51:34 +0100

    implemented menu window

---
+++ Client/Rendering/Windows/Instances/MenuWindow.cs
@@ -0,0 +1,33 @@
+﻿using ase_chess.Client.Controls.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ase_chess.Client.Controls;
+
+namespace ase_chess.Client.Rendering.Windows.Instances
+{
+    public class MenuWindow : Window, IControllable
+    {
+        public MenuWindow() : base(64, 32)
+        {
+
+        }
+
+        public void OnInput(Control sender, InputArguments args)
+        {
+            switch (args.key)
+            {
+                case '\u2191':
+                    // TODO: Navigate up
+                    args.handled = true;
+                    break;
+                case '\u2193':
+                    // TODO: Navigate down
+                    args.handled = true;
+                    break;
+            }
+        }
+    }
+}
