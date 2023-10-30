c0f4f2534574f7265f0867fbb123c29bbb3e1db3
Benedikt Müll <benedikt.muell@zimmer-group.com>
2023-10-30 11:56:34 +0100

    added basic WebSocket server

---
+++ Server/ServerSocket.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ase_chess.Server
+{
+    public class ServerSocket<T> : WebSocket, IDisposable
+    {
+        private readonly WebSocket _socket;
+        private WebSocketServer<T>? _server;
+
+        public WebSocketServer<T>? server
+        {
+            set
+            {
+                if (_server is not null)
+                {
+                    _server.SocketConnected -= OnConnected;
+                    _server.SocketDisconnected -= OnDisconnected;
+                    _server.SocketMessage -= OnMessage;
+                    _server.SocketError -= OnError;
+                }
+
+                _server = value;
+
+                if (_server is not null)
+                {
+                    _server.SocketConnected += OnConnected;
+                    _server.SocketDisconnected += OnDisconnected;
+                    _server.SocketMessage += OnMessage;
+                    _server.SocketError += OnError;
+                }
+            }
+            get => _server;
+        }
+
+        public override WebSocketState State => _socket.State;
+
+        public override WebSocketCloseStatus? CloseStatus => _socket.CloseStatus;
+        public override string? CloseStatusDescription => _socket.CloseStatusDescription;
+        public override string? SubProtocol => _socket.SubProtocol;
+
+        public delegate void ConnectedHandler();
+        public event ConnectedHandler Connected;
+
+        public delegate void DisconnectedHandler();
+        public event DisconnectedHandler Disconnected;
+
+        public delegate void MessageHandler(T message);
+        public event MessageHandler Message;
+
+        public delegate void ErrorHandler(Exception exception);
+        public event ErrorHandler Error;
+
+        public ServerSocket(WebSocket socket)
+        {
+            _socket = socket;
+        }
+
+        public ServerSocket(WebSocket socket, WebSocketServer<T>? server)
+        {
+            _socket = socket;
+            this.server = server;
+        }
+
+        private void OnConnected(ServerSocket<T> socket)
+        {
+            if (socket == this) Connected?.Invoke();
+        }
+
+        private void OnDisconnected(ServerSocket<T> socket)
+        {
+            if (socket == this) Disconnected?.Invoke();
+        }
+
+        private void OnMessage(ServerSocket<T> socket, T message)
+        {
+            if (socket == this) Message?.Invoke(message);
+        }
+
+        private void OnError(ServerSocket<T> socket, Exception exception)
+        {
+            if (socket == this) Error?.Invoke(exception);
+        }
+
+        public override void Abort()
+        {
+            _socket.Abort();
+        }
+
+        public override Task CloseAsync(WebSocketCloseStatus closeStatus, string? statusDescription, CancellationToken cancellationToken)
+        {
+            return _socket.CloseAsync(closeStatus, statusDescription, cancellationToken);
+        }
+
+        public override Task CloseOutputAsync(WebSocketCloseStatus closeStatus, string? statusDescription, CancellationToken cancellationToken)
+        {
+            return _socket.CloseOutputAsync(closeStatus, statusDescription, cancellationToken);
+        }
+
+        public override void Dispose()
+        {
+            _socket.Dispose();
+        }
+
+        public override Task<WebSocketReceiveResult> ReceiveAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken)
+        {
+            return _socket.ReceiveAsync(buffer, cancellationToken);
+        }
+
+        public override Task SendAsync(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage, CancellationToken cancellationToken)
+        {
+            return _socket.SendAsync(buffer, messageType, endOfMessage, cancellationToken);
+        }
+
+        public void Send(T message)
+        {
+            server?.Send(this, message);
+        }
+    }
+}
---
+++ Server/WebSocketServer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ase_chess.Server
+{
+    public class WebSocketServer<T>
+    {
+        private HttpListener httpListener = new HttpListener();
+        private Mutex signal = new Mutex();
+
+        private List<ServerSocket<T>> sockets = new List<ServerSocket<T>>();
+
+        public delegate void SocketConnectedHandler(ServerSocket<T> socket);
+        public event SocketConnectedHandler SocketConnected;
+
+        public delegate void SocketDisconnectedHandler(ServerSocket<T> socket);
+        public event SocketDisconnectedHandler SocketDisconnected;
+
+        public delegate void SocketMessageHandler(ServerSocket<T> socket, T message);
+        public event SocketMessageHandler SocketMessage;
+
+        public delegate void SocketErrorHandler(ServerSocket<T> socket, Exception exception);
+        public event SocketErrorHandler SocketError;
+
+        public WebSocketServer(string uri)
+        {
+            httpListener.Prefixes.Add(uri);
+        }
+
+        public WebSocketServer(int port)
+        {
+            httpListener.Prefixes.Add($"http://localhost:{port}/");
+        }
+
+        public async Task Start()
+        {
+            httpListener.Start();
+            while (signal.WaitOne())
+            {
+                Listener();
+            }
+        }
+
+        public void Send(WebSocket socket, T payload)
+        {
+            if (socket.State != WebSocketState.Open) return;
+            string message = JsonSerializer.Serialize<T>(payload);
+            socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message)), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public void Broadcast(T message)
+        {
+            foreach (var socket in sockets)
+            {
+                Send(socket, message);
+            }
+        }
+
+        private async Task Listener()
+        {
+            HttpListenerContext context = await httpListener.GetContextAsync();
+            if (context.Request.IsWebSocketRequest)
+            {
+                HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+                ServerSocket<T> socket = new ServerSocket<T>(webSocketContext.WebSocket, this);
+                sockets.Add(socket);
+                SocketConnected?.Invoke(socket);
+                while (socket.State == WebSocketState.Open)
+                {
+                    var buffer = new ArraySegment<byte>(new byte[1024]);
+                    WebSocketReceiveResult result;
+                    var allBytes = new List<byte>();
+
+                    do
+                    {
+                        result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                        for (int i = 0; i < result.Count; i++)
+                        {
+                            allBytes.Add(buffer.Array[i]);
+                        }
+                    }
+                    while (!result.EndOfMessage);
+
+                    var text = Encoding.UTF8.GetString(allBytes.ToArray(), 0, allBytes.Count);
+                    try
+                    {
+                        var payload = JsonSerializer.Deserialize<T>(text);
+                        SocketMessage?.Invoke(socket, payload);
+                    }
+                    catch (Exception ex)
+                    {
+                        SocketError?.Invoke(socket, ex);
+                    }
+                }
+                sockets.Remove(socket);
+                SocketDisconnected?.Invoke(socket);
+            }
+            signal.ReleaseMutex();
+        }
+    }
+}
